af178e0f8e2b53820a813eb132655bc9f3a4a2c0
radchukd <radchuk.d@gmail.com>
2020-03-17 08:42:40 +0100

    add request loop

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
 
 const Apify = require('apify');
 const ApifyClient = require('apify-client');
-const httpRequest = require('@apify/http-request');
+const retry = require('async-retry');
 
 Apify.main(async () => {
     const input = await Apify.getInput();
-    const { memory, useClient, fields, maxItems } = input;
+    const { memory, fields, maxItems } = input;
     const token = process.env.APIFY_TOKEN;
     const userId = process.env.APIFY_USER_ID;
     const taskId = process.env.APIFY_ACTOR_TASK_ID;
 
-    return Promise.resolve()
-        .then(() => {
-            if (useClient) {
-                const apifyClient = new ApifyClient({
-                    userId,
-                    token,
-                });
-                return apifyClient.tasks.runTask({
-                    taskId,
-                    token,
-                    memory,
-                    timeout: 0,
-                    waitForFinish: 120,
-                });
-            }
-            const runTaskUrl = `https://api.apify.com/v2/actor-tasks/${taskId}/run-sync?token=${token}&ui=1`;
-            return httpRequest({
-                url: runTaskUrl,
-                method: 'POST',
-                timeoutSecs: 720,
-                body: { memory },
-            });
-        })
-        .then(async () => {
-            const lastRunUrl = `https://api.apify.com/v2/actor-tasks/${taskId}/runs/last/dataset/`
-                                + `items?token=${token}&format=csv&limit=${maxItems}&fields=${fields}`;
-            const { body } = await httpRequest({ url: lastRunUrl });
-            await Apify.setValue('OUTPUT', { output: body });
+    const apifyClient = new ApifyClient({
+        userId,
+        token,
+    });
+    const { id, actId } = await apifyClient.tasks.runTask({
+        taskId,
+        token,
+        memory,
+        timeout: 0,
+    });
+
+    const datasetId = await retry(async () => {
+        const res = await apifyClient.acts.getRun({
+            runId: id,
+            actId,
         });
+
+        if (!res.finishedAt) {
+            throw new Error('not finished');
+        }
+        return res.defaultDatasetId;
+    }, { retries: 15, minTimeout: 20000 });
+
+    const { items } = await apifyClient.datasets.getItems({
+        format: 'csv',
+        limit: maxItems,
+        datasetId,
+        fields,
+    });
+
+    await Apify.setValue('OUTPUT', { output: items });
 });
